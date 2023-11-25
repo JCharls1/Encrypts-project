8cb46d3ba87dc7bd015c5cd87556d36fc3626eb0
charles <garciajohncharles33@gmail.com>
2023-11-25 19:15:26 +0800

    fixed a bug and added a feature

--- CodeEdit/src/Encrypt.jsx
+++ CodeEdit/src/Encrypt.jsx
@@ -3,27 +3,32 @@ function Encrypt({text}){
     let ip = 0;
     let accumulator = 0;
     let input = "";
+    let a = "asda";
     while(ip < text.length){
         let instruction = text.charAt(ip);
         
         if(instruction == 'i'){
             if(accumulator > 26)
-                accumulator = 26;
+                accumulator = 0;
             accumulator++;
         }else if(instruction == 'o'){
             if(accumulator < 0)
-                accumulator = 0;
+                accumulator = 26;
             accumulator--;
         }else if(instruction == 'r'){
-            if(accumulator > 26)
-                accumulator = 26;
             accumulator = accumulator * 2;
+            while(accumulator > 26){
+                accumulator = accumulator - 26;
+            }
+            console.log(accumulator);
         }else if(instruction == 'd'){
             if(accumulator > 26)
-                accumulator = 26;
+                accumulator = 0;
             input += alphabet[accumulator-1];
+        }else if(instruction == 'e'){
+            accumulator = 0;
         }
-
+        
         ip++;
     }
 
