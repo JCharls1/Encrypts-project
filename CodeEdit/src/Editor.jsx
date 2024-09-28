d3dfa520078e0d9bcd56bfe42ef1de1374c755ec
JCharls1 <151930727+JCharls1@users.noreply.github.com>
2024-09-28 23:59:40 +0800

    update

--- CodeEdit/src/Editor.jsx
+++ CodeEdit/src/Editor.jsx
@@ -1,54 +1,60 @@
-import { useState } from 'react'
-import './Editor.css'
-import Encrypt from './Encrypt'
-import Decrypt from './Decrypt'
+import { useState } from "react";
+import "./Editor.css";
+import Encrypt from "./Encrypt";
+import Decrypt from "./Decrypt";
 
-function Editor(){
-    const [DecryptTxt, setDecryptTxt] = useState("");
-    const [EncryptTxt, setEncryptTxt] = useState("");
-    const text = "Test Result";
-    let test = "test cosmmit"
+function Editor() {
+  const [DecryptTxt, setDecryptTxt] = useState("");
+  const [EncryptTxt, setEncryptTxt] = useState("");
+  const text = "Test Result";
+  let test = "test cosmmit";
 
-    const handleChangeDecrypt = (event) =>{
-        setDecryptTxt(event.target.value);
-    }
+  const handleChangeDecrypt = (event) => {
+    setDecryptTxt(event.target.value);
+  };
 
-    const handleChangeEncrypt = (event) =>{
-        setEncryptTxt(event.target.value);
-    }
+  const handleChangeEncrypt = (event) => {
+    setEncryptTxt(event.target.value);
+  };
 
-    return(
-        <>
-            <div className="body">
-                <div className="code-editor">
-                    <div className="code">
-                        <div className="html-code">
-                            <h1>Decrypt</h1>
-                            <textarea onChange={handleChangeDecrypt} value={DecryptTxt}></textarea>
-                        </div>
-                    </div>
-                    <pre id="result">
-                        {/* <Encrypt text={txt} />  */}
-                        <Decrypt text={DecryptTxt} /> 
-                    </pre>
-                </div>
+  return (
+    <>
+      <div className="body">
+        <div className="code-editor">
+          <div className="code">
+            <div className="html-code">
+              <h1>Encrypt</h1>
+              <textarea
+                onChange={handleChangeDecrypt}
+                value={DecryptTxt}
+              ></textarea>
             </div>
-            <div className="body">
-                <div className="code-editor">
-                    <div className="code">
-                        <div className="html-code">
-                            <h1>Encrypt</h1>
-                            <textarea onChange={handleChangeEncrypt} value={EncryptTxt}></textarea>
-                        </div>
-                    </div>
-                    <pre id="result">
-                        <Encrypt text={EncryptTxt} /> 
-                        {/* <Decrypt text={txt} />  */}
-                    </pre>
-                </div>
+          </div>
+          <pre id="result">
+            {/* <Encrypt text={txt} />  */}
+            <Decrypt text={DecryptTxt} />
+          </pre>
+        </div>
+      </div>
+      <div className="body">
+        <div className="code-editor">
+          <div className="code">
+            <div className="html-code">
+              <h1>Decrypt</h1>
+              <textarea
+                onChange={handleChangeEncrypt}
+                value={EncryptTxt}
+              ></textarea>
             </div>
-        </>
-    );
+          </div>
+          <pre id="result">
+            <Encrypt text={EncryptTxt} />
+            {/* <Decrypt text={txt} />  */}
+          </pre>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Editor;
